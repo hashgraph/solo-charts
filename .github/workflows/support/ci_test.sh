4ae6583214fb96dccf18351ad91fc68a00123083
JeffreyDallas <39912573+JeffreyDallas@users.noreply.github.com>
2024-10-02 15:23:32 -0500

    chore: using solo-containers for docker image (#9)
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>
    Signed-off-by: instamenta <instamenta@abv.bg>
    Signed-off-by: JeffreyDallas <39912573+JeffreyDallas@users.noreply.github.com>
    Co-authored-by: instamenta <instamenta@abv.bg>
    Co-authored-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- .github/workflows/support/ci_test.sh
+++ .github/workflows/support/ci_test.sh
@@ -50,9 +50,9 @@ echo "Additional values: ${CHART_VALUES_FILES}"
 echo "-----------------------------------------------------------------------------------------------------"
 if [ "${SCRIPT_NAME}" = "nmt-install.sh" ]; then
 if [[ -z "${CHART_VALUES_FILES}" ]]; then
-  helm install "${RELEASE_NAME}" -n "${NAMESPACE}" "${CHART_DIR}" --set defaults.root.image.repository=hashgraph/full-stack-testing/ubi8-init-dind
+  helm install "${RELEASE_NAME}" -n "${NAMESPACE}" "${CHART_DIR}" --set defaults.root.image.repository=hashgraph/solo-containers/ubi8-init-dind
 else
-  helm install "${RELEASE_NAME}" -n "${NAMESPACE}"  "${CHART_DIR}" -f "${CHART_DIR}/values.yaml" --values "${CHART_VALUES_FILES}" --set defaults.root.image.repository=hashgraph/full-stack-testing/ubi8-init-dind
+  helm install "${RELEASE_NAME}" -n "${NAMESPACE}"  "${CHART_DIR}" -f "${CHART_DIR}/values.yaml" --values "${CHART_VALUES_FILES}" --set defaults.root.image.repository=hashgraph/solo-containers/ubi8-init-dind
 fi
 else
 if [[ -z "${CHART_VALUES_FILES}" ]]; then
