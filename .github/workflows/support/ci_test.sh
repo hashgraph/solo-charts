9ac1a319cf1eb78b01ae447416e5b62b00983ef2
Jeromy Cannon <jeromy@swirldslabs.com>
2025-01-07 18:07:36 +0000

    set pipefail on ci test
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- .github/workflows/support/ci_test.sh
+++ .github/workflows/support/ci_test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -xeo pipefail
 
 echo "-----------------------------------------------------------------------------------------------------"
 echo "Setting up environment variables"
